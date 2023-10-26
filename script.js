31c28fc96e93ba618a9e258346e31b6bb741b804
arzumammadov1 <mi7fqqg3l@code.edu.az>
2023-10-26 08:58:16 +0400

    js.task

---
+++ script.js
@@ -0,0 +1,47 @@
+
+// Task 1
+// const params = [2, 3, 4, 4, 45, 8]
+// function siyahi(...params) {
+//     for (let i = 0; i < params.length; i++) {
+//         if (params[i] === 45) {
+//             console.log(i);
+//         }
+//     }
+// }
+// siyahi(...params)
+
+
+
+
+// Task 2
+ 
+
+
+
+
+
+
+
+
+
+
+// Task 3
+// let eb=0
+// let ek=0
+// function findboyuk(...params) {
+//     for (let i = 0; i < params.length; i++) {
+//         if (params[i] > eb) {
+//             eb=params[i]
+//         }
+//         if (params[i]<ek) {
+//             ek=params[i]
+            
+//         }
+
+//     }
+
+//     return eb
+// }
+// console.log(findboyuk(2,3,4,5,100,90));
+// console.log(ek);
+
